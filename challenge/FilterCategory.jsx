595ec06acc9e46addd950cb8027329e57af7925e
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-10-16 12:15:21 +0100

    first challenge

---
+++ challenge/FilterCategory.jsx
@@ -0,0 +1,21 @@
+// Get all unique categories from the array of dishes
+const categories = [...new Set(dishes.map((dish) => dish.category))];
+
+import dishes from "./data.js"
+export default function FilterCategory() {
+    return (
+        <fieldset>
+              <legend>Category</legend>
+              <label htmlFor="all">
+                <input type="radio" name="categories" id="all" value="all" />
+                all
+              </label>
+              {categories.map((c) => (
+                <label htmlFor={c} key={c}>
+                  <input type="radio" name="categories" id={c} value={c} />
+                  {c}
+                </label>
+              ))}
+            </fieldset>
+    )
+}
---
+++ challenge/ListDishes.jsx
@@ -0,0 +1,18 @@
+import dishes from "./data.js";
+
+
+export default function ListDishes () {
+    return (
+        <section aria-label="dishes">
+        <ul className="grid">
+          {dishes.map((dish) => (
+            <li key={dish.id} className="card">
+              <h3>{dish.name}</h3>
+              {dish.description && <p>{dish.description}</p>}
+              <div>£{dish.price.toFixed(2)}</div>
+            </li>
+          ))}
+        </ul>
+      </section>
+    )
+}
